7e0a0da995d3f42bd19f59e4701dc9fe28bc7cf5
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2021-12-16 22:13:35 +0300

    add profile model

--- awards/models.py
+++ awards/models.py
@@ -2,6 +2,9 @@ from django.db import models
 import datetime as dt
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField  
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 # Create your models here.
 class Website(models.Model):
@@ -11,3 +14,24 @@ class Website(models.Model):
     country = models.CharField(max_length = 120)
     landing_page = CloudinaryField('Landing page image')
     uploaded_on = models.DateTimeField(auto_now_add=True)
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, primary_key = True, verbose_name = 'user', related_name = 'profile', on_delete = models.CASCADE)
+    full_name = models.CharField(max_length=30)
+    bio = HTMLField(blank=True, null=True)
+    profile_pic = CloudinaryField('image')
+    followers = models.ManyToManyField(User, blank = True, related_name = 'followers')
+
+    @classmethod
+    def search_user(cls,search_term):
+        users = cls.objects.filter(user__username = search_term)
+        return users
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User) 
+def save_user_Profile(sender, instance, **kwargs):
+    instance.profile.save()     
