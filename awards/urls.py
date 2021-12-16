923ad24941b57aa1bed25b289d918bc3548dd020
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2021-12-16 21:43:39 +0300

    add website template and view

--- awards/urls.py
+++ awards/urls.py
@@ -1,7 +1,9 @@
 from django.conf import settings
 from django.conf.urls import url
 from . import views
+from .views import WebsiteListView
 
 urlpatterns = [
     url('^$', views.index, name  = 'index'),
+    url('site_of_day/', WebsiteListView.as_view(), name='site_of_day'),
 ]    
--- awards/views.py
+++ awards/views.py
@@ -1,5 +1,43 @@
 from django.shortcuts import render, redirect
+from django.views import View
+from .models import Website
+
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html') 
+
+
+class WebsiteListView(View):
+    def get(self, request):
+        websites = Website.objects.all().order_by('-uploaded_on')
+        form = SiteForm()
+
+        context = {
+            'site_list':websites,
+            'form':form,
+        }
+
+        return render(request, 'website_of_day.html', context) 
+
+    def post(self, request):
+        websites = Website.objects.all().order_by('-uploaded_on')
+        site_of_day = Website.objects.all().order_by('-uploaded_on').first()
+        form = SiteForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            new_site = form.save(commit = False)
+            new_site.author = request.user
+
+            if 'img' in request.FILES:
+                new_site.image = request.FILES['img']
+
+            new_site.save()   
+
+        context = {
+            'site_list':websites,
+            'site_of_day':site_of_day,
+            'form':form,
+        }
+
+        return render(request, 'website_of_day.html', context)       
