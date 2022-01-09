5fd6e8010d8d920bc255fb13b115392d2739c1a2
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2022-01-09 21:28:24 +0300

    styling

--- awards/tests.py
+++ awards/tests.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from .models import Website, UserProfile
 
 # Create your tests here.
-class ProjectTestClass(TestCase):  # Project class test
+class WebsiteTestClass(TestCase):  # Website class test
     def setUp(self):
         # creating a user
         user = User.objects.create(
@@ -23,19 +23,19 @@ class ProjectTestClass(TestCase):  # Project class test
     #    self.website.save_website()
     #    websites = Website.objects.all()
     #    self.assertTrue(len(websites) > 0)
-    class ProfileTestClass(TestCase):
-        def setUp(self):
+class ProfileTestClass(TestCase):
+    def setUp(self):
         
-            user = User.objects.create(
-                username="test_user", first_name="site", last_name="awards"
-            )
+        user = User.objects.create(
+            username="test_user", first_name="site", last_name="awards"
+        )
 
-            self.profile = UserProfile(
-                bio="Test Bio",
-                full_name = "Daniel Kariuki",
-                user=user,
-                profile_pic = "image.jpg",
-                followers = ""
-            )
-        def test_instance(self):
-            self.assertTrue(isinstance(self.profile, UserProfile))
+        self.profile = UserProfile(
+            bio="Test Bio",
+            full_name = "Daniel Kariuki",
+            user=user,
+            profile_pic = "image.jpg",
+            #followers = "followers.set()"
+        )
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile, UserProfile))
