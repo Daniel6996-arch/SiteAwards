28ba53d1da5375efa47fe28db1f0d338589df53f
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2022-01-10 12:09:14 +0300

    update urls

--- awards/urls.py
+++ awards/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls import url
 from django.urls import path
 from . import views
-from .views import WebsiteListView, ProfileView, ProfileEditView, WebsiteDetailView, UserList, SiteList, Usability, Design
+from .views import WebsiteListView, ProfileView, ProfileEditView, WebsiteDetailView, UserList, SiteList, Usability, Design, Content
 
 urlpatterns = [
     url('^$', views.index, name  = 'index'),
